65b5347736612684c9a5ac643123e7d7733ae87e
haykavagya <haikavaks@gmail.com>
2020-08-02 17:20:24 +0400

    added some migrations

--- db.ts
+++ db.ts
@@ -4,7 +4,7 @@ import { config } from 'https://deno.land/x/dotenv/mod.ts';
 const env = config()
 
 export const db = new Database('postgres', {
-    host: env.DATABASE_URL || 'localhost',
+    host: 'localhost',
     username: 'postgres',
     password: 'devIntelinair1989',
     database: 'withoutbug',
